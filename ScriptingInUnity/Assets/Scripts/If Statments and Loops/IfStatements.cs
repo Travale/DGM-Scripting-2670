0e1b8b9917abac5170824069e98870b708795512
Travis <supafatcats@hotmail.com>
2018-10-09 17:35:05 -0600

    working on switch

--- ScriptingInUnity/Assets/Scripts/If Statments and Loops/IfStatements.cs
+++ ScriptingInUnity/Assets/Scripts/If Statments and Loops/IfStatements.cs
@@ -1,45 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.UI;
 
 public class IfStatements : MonoBehaviour
 {
-    public int sum = 0;
-    public Rigidbody bullet;
-    public int rage = 7;
+    public int sum; 
+    public int rage;
+    public GameObject bulletPrefab;
+    public Transform spawnBullet;
+    private int count;
+    public Message angryMessage = null;
         
     private void Start()
     {
-        if(sum == 10 - 10 )
+
+        for (rage = 0; rage < 10; rage++)
         {
-            switch (rage)
+
+            sum = 0;
+            if (sum == 10 - 10)
             {
-                case 7:
-                    print("You threw a chair");
-                    break;
-                    
-                case 5:
-                    print("You punched a wall");
-                    break;
-                    
-                case 3: 
-                    print("You violently ate a snickers bar");
-                    break;
-                default:
-                    print("You are not angry");
-                    break;
-                
+                switch (rage)
+                {
+                    case 7:
+                        print("You threw a chair");
+                        break;
+
+                    case 5:
+                        print("You punched a wall");
+                        break;
+
+                    case 3:
+                        print("You violently ate a snickers bar");
+                        break;
+                    default:
+                        print("You are not angry");
+                        break;
+
+                }
             }
         }
     }
 
+    void Fire()
+    {
+        var bullet = Instantiate(bulletPrefab, spawnBullet.position, spawnBullet.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.up * 5;
+        Destroy(bullet, 2.0f);
+    }
+
     private void Update()
     {
-        if (Input.GetButtonDown("Shot1"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Rigidbody duplicate;
-            duplicate = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
-            duplicate.velocity = transform.TransformDirection(Vector3.up * 10);
+            Fire();
+
 
         }
     }
