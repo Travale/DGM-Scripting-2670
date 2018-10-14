3849838fe5686055f599e585e48f36599550024f
Travis <supafatcats@hotmail.com>
2018-10-13 22:44:01 -0600

    bouncy box hyper casual brainstorm

---
+++ ScriptingInUnity/Assets/Scripts/HyperCasualScripts/BouncyCube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyCube : MonoBehaviour
+{
+
+
+	public float moveSpeed = 20f;
+	private Rigidbody squareRigidBody;
+	
+	
+	void Start ()
+	{
+		squareRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	
+	void Update ()
+	{
+		float inputX = Input.GetAxis("Horizontal");
+		float inputZ = Input.GetAxis("Vertical");
+
+		float moveX = inputX * moveSpeed * Time.deltaTime;
+		float moveZ = inputZ * moveSpeed * Time.deltaTime;
+
+		//transform.Translate(moveX, 0f, moveZ);
+		
+		squareRigidBody.AddForce(moveX, 0f, moveZ);
+	}
+}
